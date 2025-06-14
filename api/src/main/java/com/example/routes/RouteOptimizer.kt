4bb055606409eb7e67df2c97f10b560a9b232f21
Max Bonas <113984950+MaxBonas@users.noreply.github.com>
2025-06-14 11:58:43 +0200

    Add initial Android project skeleton

---
+++ api/src/main/java/com/example/routes/RouteOptimizer.kt
@@ -0,0 +1,7 @@
+package com.example.routes
+
+class RouteOptimizer {
+    fun optimize(currentLat: Double, currentLng: Double, addresses: List<String>) {
+        // TODO: implement call to Google Maps Route Optimization API
+    }
+}
---
+++ core/src/main/java/com/example/routes/Route.kt
@@ -0,0 +1,8 @@
+package com.example.routes
+
+// TODO: define route fields
+class Route(
+    val addresses: List<Address>
+) {
+    // TODO: add functions to convert to GPX/KML
+}
